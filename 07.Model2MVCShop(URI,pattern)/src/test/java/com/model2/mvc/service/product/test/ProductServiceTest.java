06404aefc0a34e4c593e1f34646c4d1e54252130
bitcamp <bitcamp@DESKTOP-TVHHO4S>
2022-05-04 19:27:05 +0900

    Purchase 진행예정

---
+++ 07.Model2MVCShop(URI,pattern)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
@@ -0,0 +1,232 @@
+package com.model2.mvc.service.product.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.user.UserService;
+
+
+/*
+ *	FileName :  UserServiceTest.java
+ * ㅇ JUnit4 (Test Framework) 과 Spring Framework 통합 Test( Unit Test)
+ * ㅇ Spring 은 JUnit 4를 위한 지원 클래스를 통해 스프링 기반 통합 테스트 코드를 작성 할 수 있다.
+ * ㅇ @RunWith : Meta-data 를 통한 wiring(생성,DI) 할 객체 구현체 지정
+ * ㅇ @ContextConfiguration : Meta-data location 지정
+ * ㅇ @Test : 테스트 실행 소스 지정
+ */
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/context-common.xml",
+									"classpath:config/context-aspect.xml",
+									"classpath:config/context-mybatis.xml",
+									"classpath:config/context-transaction.xml"})
+public class ProductServiceTest {
+
+	//==>@RunWith,@ContextConfiguration 이용 Wiring, Test 할 instance DI
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+
+	//@Test
+	public void testAddProduct() throws Exception {
+		
+		Product product = new Product();
+		product.setProdNo(999);
+		product.setProdName("testProductName");
+		product.setProdDetail("testDetail");
+		product.setManuDate("99999999");
+		product.setPrice(99999);
+		product.setFileName("testFileName");
+		
+		productService.addProduct(product);
+		  
+		product = productService.getProduct(999);
+
+		//==> console 확인
+		//System.out.println(product);
+		
+		//==> API 확인
+		Assert.assertEquals(999, product.getProdNo());
+		Assert.assertEquals("testProductName", product.getProdName());
+		Assert.assertEquals("testDetail", product.getProdDetail());
+		Assert.assertEquals("99999999", product.getManuDate());
+		Assert.assertEquals(99999, product.getPrice());
+		Assert.assertEquals("testFileName", product.getFileName());
+	}
+	
+	//@Test
+	public void testGetProduct() throws Exception {
+		
+		Product product = new Product();
+		
+		product = productService.getProduct(999);
+
+		//==> console 확인
+		//System.out.println(user);
+		
+		//==> API 확인
+		Assert.assertEquals(999, product.getProdNo());
+		Assert.assertEquals("testProductName", product.getProdName());
+		Assert.assertEquals("testDetail", product.getProdDetail());
+		Assert.assertEquals("99999999", product.getManuDate());
+		Assert.assertEquals(99999, product.getPrice());
+		Assert.assertEquals("testFileName", product.getFileName());
+
+		Assert.assertNotNull(productService.getProduct(10000));
+		Assert.assertNotNull(productService.getProduct(10001));
+	}
+	
+	//@Test
+	 public void testUpdateProduct() throws Exception{
+		 
+		Product product = productService.getProduct(999);
+		Assert.assertNotNull(product);
+		
+		Assert.assertEquals("testProductName", product.getProdName());
+		Assert.assertEquals("testDetail", product.getProdDetail());
+		Assert.assertEquals("99999999", product.getManuDate());
+		Assert.assertEquals(99999, product.getPrice());
+		Assert.assertEquals("testFileName", product.getFileName());
+
+		product.setProdName("change");
+		product.setProdDetail("changeDetail");
+		product.setManuDate("77777777");
+		product.setPrice(77777);
+		product.setFileName("changeFileName");
+		
+		productService.updateProduct(product);
+		
+		product = productService.getProduct(999);
+		Assert.assertNotNull(product);
+		
+		//==> console 확인
+		//System.out.println(user);
+			
+		//==> API 확인
+		Assert.assertEquals("change", product.getProdName());
+		Assert.assertEquals("changeDetail", product.getProdDetail());
+		Assert.assertEquals("77777777", product.getManuDate());
+		Assert.assertEquals(77777, product.getPrice());
+		Assert.assertEquals("changeFileName", product.getFileName());
+	 }
+	 
+	
+	 //@Test
+	 public void testGetProductListAll() throws Exception{
+		 
+	 	Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	Map<String,Object> map = productService.getProductList(search);
+	 	
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(3, list.size());
+	 	
+		//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 	
+	 	System.out.println("=======================================");
+	 	
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("0");
+	 	search.setSearchKeyword("");
+	 	map = productService.getProductList(search);
+	 	
+	 	list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(3, list.size());
+	 	
+	 	//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 }
+	 
+	 //@Test
+	 public void testGetProdListByProdNo() throws Exception{
+		 
+	 	Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("0");
+	 	search.setSearchKeyword("10000");
+	 	Map<String,Object> map = productService.getProductList(search);
+	 	
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(1, list.size());
+	 	
+		//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 	
+	 	System.out.println("=======================================");
+	 	
+	 	search.setSearchCondition("0");
+	 	search.setSearchKeyword(""+System.currentTimeMillis());
+	 	map = productService.getProductList(search);
+	 	
+	 	list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(0, list.size());
+	 	
+		//==> console 확인
+	 	//System.out.println(list);
+	 	
+	 	totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 }
+	 
+	 @Test
+	 public void testGetProductListByProdName() throws Exception{
+		 
+	 	Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("1");
+	 	search.setSearchKeyword("자전거");
+	 	Map<String,Object> map = productService.getProductList(search);
+	 	
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(1, list.size());
+	 	
+		//==> console 확인
+	 	System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 	
+	 	System.out.println("=======================================");
+	 	
+	 	search.setSearchCondition("1");
+	 	search.setSearchKeyword(""+System.currentTimeMillis());
+	 	map = productService.getProductList(search);
+	 	
+	 	list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(0, list.size());
+	 	
+		//==> console 확인
+	 	System.out.println(list);
+	 	
+	 	totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 }	 
+}
